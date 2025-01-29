be57290525f3c2a195ba97361b82ad7a4ba4422d
Baslim <nikita7343072@gmail.com>
2025-01-29 16:19:55 +0500

    KS-2-4 the answer to the forth task of the second lesson

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val crystalOre = 7
+    val ironOre = 11
+
+    val bonusCrystalOre = crystalOre * 20 / 100
+    val bonusIronOre = ironOre * 20 / 100
+
+    println("Бонусная кристаллическая руда: $bonusCrystalOre")
+    println("Бонусная железная руда: $bonusIronOre")
+
+}
