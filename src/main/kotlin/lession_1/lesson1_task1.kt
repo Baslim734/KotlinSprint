bb72ce9b6cd2cc89f32de7b958c50cafbaff7ef8
Baslim <nikita7343072@gmail.com>
2025-01-29 22:24:35 +0500

    Рефактор по замечаниям убрал комментарии Исправил значение переменной

--- src/main/kotlin/lession_1/lesson1_task1.kt
+++ src/main/kotlin/lession_1/lesson1_task1.kt
@@ -1,11 +1,10 @@
 package org.example.lession_1
 
 fun main() {
-    // Переменная для количества заказов (целое число)
-    val orderCount: Int = 42
 
-    // Переменная для текста с благодарностью за покупку
+    val orderCount: Int = 75
     val thankYouMessage: String = "Спасибо за вашу покупку!"
 
     println("Ваш заказ из: $orderCount позиций. $thankYouMessage")
+
 }
