d637bac7503b0442ee4a3c41921e6c6cc45776e9
Baslim <nikita7343072@gmail.com>
2025-01-29 22:34:37 +0500

    Рефактор по замечаниям убрал комментарии Исправил значение переменной удалил лишний файл

--- src/main/kotlin/lession_1/lesson1_task2.kt
+++ src/main/kotlin/lession_1/lesson1_task2.kt
@@ -2,15 +2,15 @@ package org.example.lession_1
 
 fun main() {
 
-    var staffCount: Short = 1999
-    val orderCount: Int = 42
+    var staffCount: Short = 2000
+    val orderCount: Int = 75
     val thankYouMessage: String = "Спасибо за вашу покупку!"
 
 
     //   println(orderCount)
     println(thankYouMessage)
 
-    staffCount = 1998
+    staffCount = 1999
     println(staffCount)
 
 }
