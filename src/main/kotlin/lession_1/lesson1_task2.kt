86b533c7a578bce6aa123dbcd1ca776023a0aa78
Baslim <nikita7343072@gmail.com>
2025-01-29 14:56:59 +0500

    KS-1-2 the answer to the second task of the first lesson part 1

---
+++ src/main/kotlin/lession_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lession_1
+
+fun main() {
+
+    val staffCount: Int = 1999
+    val orderCount: Int = 42
+    val thankYouMessage: String = "Спасибо за вашу покупку!"
+
+     println(staffCount)
+     println(orderCount)
+     println(thankYouMessage)
+
+}
