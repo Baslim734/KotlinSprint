b73c1a0f46a89b1de6a5b53a2efcc1328bc94845
Baslim <nikita7343072@gmail.com>
2025-01-30 14:02:42 +0500

    KS-3-1 решение задания 3его урока 1го задания

---
+++ src/main/kotlin/lession_3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lession_1
+
+fun main() {
+
+    val userName = "Ричард Баслим"
+    var greetings = "Доброго утра"
+
+    println("$greetings$userName")
+
+    greetings = "Доброго вечера"
+
+    println("$greetings, $userName")
+
+}
