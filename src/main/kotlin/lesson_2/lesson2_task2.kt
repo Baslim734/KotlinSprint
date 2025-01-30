b4d5bb0c4be8b4bbe1dae8a84290089a9463571a
Baslim <nikita7343072@gmail.com>
2025-01-30 13:54:26 +0500

    Правки

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -7,12 +7,8 @@ fun main() {
     val numberOfInterns = 30
     val internSalary = 20000
 
-    // расходы на выплату зарплаты постоянных сотрудников
     val totalEmployeeSalaryExpenses = numberOfEmployees * employeeSalary
-
-    // общие расходы по зарплате после прихода стажеров
     val totalSalaryExpenses = totalEmployeeSalaryExpenses + (numberOfInterns * internSalary)
-
     val averageSalary = totalSalaryExpenses / (numberOfEmployees + numberOfInterns)
 
     println("Расходы на выплату зарплаты постоянных сотрудников: $totalEmployeeSalaryExpenses руб.")
