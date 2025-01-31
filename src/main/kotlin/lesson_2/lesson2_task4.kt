c31271d37153d7a787b74ee0c5b2f52ef50fe6e7
Baslim <nikita7343072@gmail.com>
2025-01-31 21:17:15 +0500

    Вынес магические числа

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,9 +4,11 @@ fun main() {
 
     val crystalOre = 7
     val ironOre = 11
+    val bonusPercentage =  20
+    val percentageDivider = 100
 
-    val bonusCrystalOre = crystalOre * 20 / 100
-    val bonusIronOre = ironOre * 20 / 100
+    val bonusCrystalOre = crystalOre * bonusPercentage / percentageDivider
+    val bonusIronOre = ironOre * bonusPercentage / percentageDivider
 
     println("Бонусная кристаллическая руда: $bonusCrystalOre")
     println("Бонусная железная руда: $bonusIronOre")
