5b3c235a6745c9bfd29477296e6a2d1ea19110e4
Baslim <nikita7343072@gmail.com>
2025-01-31 09:59:27 +0500

    Правки для задания 5 второго урока

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,15 +5,15 @@ fun main() {
     val principal = 70000.0
     val interestRate = 16.7 / 100
     val years = 20
+    val numberForRounding = 1000.0
 
     var futureValue = principal
 
-    // Использовал цикл как самый простой вариант
     for (i in 1..years) {
         futureValue += futureValue * interestRate
     }
 
-    val result = (futureValue * 1000).toInt() / 1000.0
+    val result = (futureValue * numberForRounding).toInt() / numberForRounding
     println("Размер вклада через $years лет: $result")
 
 }
