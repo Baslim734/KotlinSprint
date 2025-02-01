791b044ca8793a11ffe490f591546e07b258b129
Baslim <nikita7343072@gmail.com>
2025-02-01 10:36:34 +0500

    Правки в связи с коментарием

--- src/main/kotlin/lession_3/lesson3_task3.kt
+++ src/main/kotlin/lession_3/lesson3_task3.kt
@@ -3,19 +3,19 @@ package org.example.lession_3
 fun main() {
 
     val number = 6
-    val multiplicationTableStart = 1
-    val multiplicationTableEnd = 9
 
-    println("$number * 1 = ${number * 1}")
-    println("$number * 2 = ${number * 2}")
-    println("$number * 3 = ${number * 3}")
-    println("$number * 4 = ${number * 4}")
-    println("$number * 5 = ${number * 5}")
-    println("$number * 6 = ${number * 6}")
-    println("$number * 7 = ${number * 7}")
-    println("$number * 8 = ${number * 8}")
-    println("$number * 9 = ${number * 9}")
-
-    println((multiplicationTableStart..multiplicationTableEnd).joinToString("\n") { "$number x $it = ${number * it}" })
+    println(
+        """
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}
+        """.trimIndent()
+    )
 
 }
