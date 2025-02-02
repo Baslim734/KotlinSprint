a4bb52d3b28a04dd6eb423f8c141db667c32c1f7
Baslim <nikita7343072@gmail.com>
2025-02-02 09:58:25 +0500

    Правки по условиям задачи

--- src/main/kotlin/lession_3/lesson3_task5.kt
+++ src/main/kotlin/lession_3/lesson3_task5.kt
@@ -2,26 +2,9 @@ package org.example.lession_3
 
 fun main() {
 
-    var stepFrom = "E2"
-    var stepWhere = "E4"
-    var numberStep = 1
-
-    println("[$stepFrom-$stepWhere;$numberStep]")
-
-    stepFrom = "D2"
-    stepWhere = "D3"
-    numberStep ++
-
-    println("[$stepFrom-$stepWhere;$numberStep]")
-
-    stepFrom = "D2"
-    stepWhere = "D4"
-    numberStep = 0
-
-    val serverResponse = "[$stepFrom-$stepWhere;$numberStep]"
+    val serverResponse = "D2-D4;0"
 
     val cleanData = serverResponse
-        .removeSurrounding("[", "]")
         .split(";")
 
     val (parsedFrom, parsedTo) = cleanData[0].split("-")
