952f791d550767655eabd34aa828d4893997db60
Baslim <nikita7343072@gmail.com>
2025-02-03 12:26:07 +0500

    Поправил название, поправил форматирование

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_4
 
-const val SUNNY_WEATHER = true
-const val TENT_OPEN = true
+const val IS_SUNNY = true
+const val IS_TENT_OPEN = true
 const val NEEDED_HUMIDITY = 20
 const val NOT_WINTER = "зима"
 
@@ -12,7 +12,11 @@ fun main() {
     val humidity = 20
     val currentSeason = "зима"
 
-    val checkingConditions = isSunnyWeather == SUNNY_WEATHER && isTentOpen == TENT_OPEN && humidity >= NEEDED_HUMIDITY && currentSeason != NOT_WINTER
+    val checkingConditions =
+            isSunnyWeather == IS_SUNNY &&
+            isTentOpen == IS_TENT_OPEN &&
+            humidity >= NEEDED_HUMIDITY &&
+            currentSeason != NOT_WINTER
 
     println("Благоприятные ли условия сейчас для роста бобовых? $checkingConditions")
 
