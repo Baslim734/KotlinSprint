73c3d44e01197c0b1311f06c8be36c881a3d21f3
Baslim <nikita7343072@gmail.com>
2025-02-03 12:02:54 +0500

    Поправил значения для забронированных столов

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_4
 
-const val TABLES_QUANTITY = 18
+const val TABLES_QUANTITY = 13
 
 fun main() {
 
-    val todayBookedTables = 18
+    val todayBookedTables = 13
     val yesterdayBookedTables = 9
 
     println("[Доступность столов на сегодня:${todayBookedTables < TABLES_QUANTITY}]")
