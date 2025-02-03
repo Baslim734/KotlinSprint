3044be60a96567450a41e665ed02553f7e14d937
Baslim <nikita7343072@gmail.com>
2025-02-03 12:57:56 +0500

    Поправил названия

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -4,19 +4,19 @@ package org.example.lesson_4
 fun main() {
 
     println("Введите наличие повреждений (true/false):")
-    val hasDamage = readlnOrNull()?.toBoolean() ?: false
+    val isDamaged = readlnOrNull()?.toBoolean() ?: false
 
     println("Введите текущий состав экипажа:")
     val crewSize = readlnOrNull()?.toIntOrNull() ?: 0
 
     println("Введите количество ящиков с провизией:")
-    val provisions = readlnOrNull()?.toIntOrNull() ?: 0
+    val provisionsNumber = readlnOrNull()?.toIntOrNull() ?: 0
 
     println("Введите, благоприятны ли метеоусловия (true/false):")
     val isWeatherFavorable = readlnOrNull()?.toBoolean() ?: false
 
-    val canSetSail = (!hasDamage && crewSize in 55..70 && provisions > 50 && isWeatherFavorable) ||
-            (hasDamage && crewSize == 70 && isWeatherFavorable && provisions >= 50)
+    val canSetSail = (!isDamaged && crewSize in 55..70 && provisionsNumber > 50 && isWeatherFavorable) ||
+            (isDamaged && crewSize == 70 && isWeatherFavorable && provisionsNumber >= 50)
 
     if (canSetSail) {
         println("Корабль может отплыть в плавание.")
