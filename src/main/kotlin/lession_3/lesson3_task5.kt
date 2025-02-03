b0eaa4b206cf9042031d62feb3b3f85b7b6681cd
Baslim <nikita7343072@gmail.com>
2025-02-03 11:59:41 +0500

    Поправил использование split

--- src/main/kotlin/lession_3/lesson3_task5.kt
+++ src/main/kotlin/lession_3/lesson3_task5.kt
@@ -4,12 +4,8 @@ fun main() {
 
     val serverResponse = "D2-D4;0"
 
-    val cleanData = serverResponse
-        .split(";")
-
-    val (parsedFrom, parsedTo) = cleanData[0].split("-")
-
-    val parsedNumber = cleanData[1]
+    val (parsedFrom, parsedTo,parsedNumber) = serverResponse
+        .split(";","-")
 
     println("\nРезультат парсинга:")
     println("Откуда: $parsedFrom")
